efe348270116d1586c5ccc6527f44d7b0a33f3a6
TheGodOfAct <aris862@hotmail.com>
2018-07-04 12:36:09 +0300

    Updated clock.vhd

--- clock.vhd
+++ clock.vhd
@@ -1,13 +1,3 @@
----clk_process :process
---   begin
---        clk <= '0';
---        wait for clk_period/2;  --for 0.5 ns signal is '0'.
---        clk <= '1';
---        wait for clk_period/2;  --for next 0.5 ns signal is '1'.
---   end process;
-
---END;
-
 library ieee;
 use ieee.std_logic_1164.all;
 
