1b45ae0c11a7e2e1be4c7fa9e8a44d065d8a929b
TheGodOfAct <aris862@hotmail.com>
2018-07-04 14:21:41 +0300

    Progressing with the structural
    preparing the possible necessary components for the structural implementation

--- booth_structural.vhd
+++ booth_structural.vhd
@@ -24,7 +24,21 @@ component clock
 	(
 		clk: out std_logic
 	);
-end clock;
+end component;
 
+--	the adder for adding the Accumulator
 component full_adder_4bit
 	port
+	(
+		a, b: in std_logic_vector(3 downto 0);
+		cin: in std_logic;
+		sum: out std_logic_vector(4 downto 0);
+		carry: inout std_logic
+	);
+end component;
+
+signal complement_m: std_logic_vector(3 downto 0);
+signal acc: std_logic_vector(3 downto 0);
+signal aqqm: std_logic_vector(8 downto 0);
+signal qm: std_logic;
+
---
+++ or2.vhd
@@ -0,0 +1,18 @@
+-- work package #2, full-adder part
+-- component for or gate
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity or2 is
+	port
+	(
+		i1, i2: in std_logic;
+		o1: out std_logic
+	);
+end or2;
+
+architecture arch of or2 is
+	begin
+		o1 <= i1 or i2;
+end arch;
