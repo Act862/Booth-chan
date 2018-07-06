e9dd39af7f0da1403e0bb95bfd7d474a5cfc9d2c
TheGodOfAct <aris862@hotmail.com>
2018-07-06 12:36:58 +0300

    Updates
    Wrote the port mapping in the concurrent block

--- booth_structural.vhd
+++ booth_structural.vhd
@@ -68,7 +68,7 @@ end component;
 component mux4to1
 	port
 	(
-		a, b: in std_logic_vector(3 downto 0);
+		a, b, c, d: in std_logic_vector(3 downto 0);
 		slc: in std_logic_vector(1 downto 0);
 		output: out std_logic_vector(3 downto 0)
 	);
@@ -86,9 +86,22 @@ end component;
 
 -- signal declarations
 signal clk: std_logic;
-signal select1, 
+signal qm: std_logic := '0';
+signal select1, continue: std_logic;
+signal phase_result: std_logic_vector(3 downto 0) := "0000";
+signal pipo_out, sub_res, sum_res, A_new: std_logic_vector(3 downto 0);
+signal conc: std_logic_vector(8 downto 0);
 --	begin the architecture
 begin
 	clk_sig: clock port map(clk);	--	we have taken the code
-	count: reverse_counter port map(size, set_rc, 
-	s1: mux2to1 port map(clk, '0', 
+	count: reverse_counter port map(size, set_rc, size, continue);
+	s1: mux2to1 port map(clk, '0', continue, select1);
+	acc: pipo port map(select1, set_rc, phase_result, pipo_out);
+	sum: full_adder_4bit port map(pipo_out, m, '0', sum_res); 
+	sub: subtractor_4bit port map(pipo_out, m, sub_res);
+	c: conc <= pipo_out & q & qm;
+	s2: mux4to1 port map(pipo_out, sum_res, sub_res, pipo_out, conc(1 downto 0), A_new);
+	c2: conc <= A_new & conc(4 downto 0);
+	shift: right_shifter port map(conc, conc);
+	phase_result <= conc;
+end structural;
